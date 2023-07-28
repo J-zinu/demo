62f71d0e5a12709c57d362e93d5f230eb9106e61
J-zinu <wjswlsdn4053@naver.com>
2023-07-28 16:09:21 +0900

    각각 콘솔을 찍었음 알아보기힘들어서

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,3 +1,36 @@
+//package com.example.demo.controller;
+//
+//import com.example.demo.dto.MemberDTO;
+//import com.example.demo.service.LoginService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.servlet.http.HttpSession;
+//@Controller
+//@RequestMapping
+//@RequiredArgsConstructor
+//public class LoginController {
+//    private final LoginService loginService;
+//
+//    @GetMapping("/login")
+//    public String loginForm() {
+//        return "/login";
+//    }
+//
+//
+//    @PostMapping("/login")
+//    public String login(@ModelAttribute MemberDTO memberDTO,
+//                        HttpSession session) {
+//        boolean loginResult = loginService.login(memberDTO);
+//        if (loginResult) {
+//            session.setAttribute("user_id", memberDTO.getUser_id());
+//            return "redirect:/todoList";
+//        } else {
+//            return "/login";
+//        }
+//    }
+//}
 package com.example.demo.controller;
 
 import com.example.demo.dto.MemberDTO;
@@ -7,6 +40,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping
 @RequiredArgsConstructor
@@ -15,17 +49,22 @@ public class LoginController {
 
     @GetMapping("/login")
     public String loginForm() {
+        System.out.println("로그인 폼에 접근");
         return "/login";
     }
 
     @PostMapping("/login")
     public String login(@ModelAttribute MemberDTO memberDTO,
                         HttpSession session) {
+        System.out.println("사용자 ID로 로그인 시도 중:" + memberDTO.getUser_id());
+
         boolean loginResult = loginService.login(memberDTO);
         if (loginResult) {
             session.setAttribute("user_id", memberDTO.getUser_id());
+            System.out.println("로그인 성공, 작업관리 목록으로 리디렉션합니다.");
             return "redirect:/todoList";
         } else {
+            System.out.println("로그인 실패, 로그인 페이지로 돌아가기");
             return "/login";
         }
     }
